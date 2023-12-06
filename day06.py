50963e6aa7ebe8cabdf977c9ed6c2b63cb76db07
Conrad Rushing <conrad.rushing@gmail.com>
2023-12-06 11:21:48 -0500

    Done! Wow that was easier

---
+++ day06.py
@@ -0,0 +1,188 @@
+# adventofcode 2023
+# crushallhumans
+# puzzle 6
+# 12/6/2023
+
+import os
+import re
+import sys
+import math
+import unittest
+import socket
+import hashlib
+import pprint
+import random
+from functools import reduce
+from functools import reduce
+from functools import cache
+from itertools import chain
+from multiprocessing import Pool
+pp = pprint.PrettyPrinter()
+
+DEBUG = False
+
+def one_star(param_set, is_two_star = False):
+    print("---------------one_star--------------------")
+    param_set = reprocess_input(param_set)
+    winners = []
+    for i in param_set[0]:
+        winner = 0
+        for j in range(0,i[0]):
+            if calc_distance(j, i[0], i[1]):
+                winner += 1
+        winners.append(winner)
+    return reduce(lambda x,y: x*y, winners)
+
+
+def two_star(param_set):
+    print("---------------two_star--------------------")
+    param_set = reprocess_input(param_set)
+    print(param_set)
+    winner = 0
+    race_time = param_set[1][0]
+    race_record = param_set[1][1]
+    for j in range(0,race_time):
+        if not calc_distance(j, race_time, race_record):
+            winner += 1
+        else:
+            break
+    return race_time - (winner * 2) + 1
+
+
+def reprocess_input(param_set):
+    if isinstance(param_set,str):
+        l = []
+        l = [input_line.strip() for input_line in param_set.splitlines()]
+        param_set = l
+    races = list(zip(
+        list(int(x) for x in re.split(r'\s+',param_set[0])[1:]),
+        list(int(y) for y in re.split(r'\s+',param_set[1])[1:])
+    ))
+    race_one = [
+        int(''.join(re.split(r'\s+',param_set[0])[1:])),
+        int(''.join(re.split(r'\s+',param_set[1])[1:]))
+    ]
+    return [races, race_one]
+
+def calc_distance(hold_length: int, race_length: int, race_record: int, hold_step: int = 1) -> bool:
+    distance = (hold_length * hold_step) * (race_length - hold_length)
+    if DEBUG: print('race ',hold_length,race_length,distance,race_record)
+    return distance > race_record
+
+def puzzle_text():
+    print("""--- Day 6: Wait For It ---
+The ferry quickly brings you across Island Island. After asking around, you discover that there is indeed normally a large pile of sand somewhere near here, but you don't see anything besides lots of water and the small island where the ferry has docked.
+
+As you try to figure out what to do next, you notice a poster on a wall near the ferry dock. "Boat races! Open to the public! Grand prize is an all-expenses-paid trip to Desert Island!" That must be where the sand comes from! Best of all, the boat races are starting in just a few minutes.
+
+You manage to sign up as a competitor in the boat races just in time. The organizer explains that it's not really a traditional race - instead, you will get a fixed amount of time during which your boat has to travel as far as it can, and you win if your boat goes the farthest.
+
+As part of signing up, you get a sheet of paper (your puzzle input) that lists the time allowed for each race and also the best distance ever recorded in that race. To guarantee you win the grand prize, you need to make sure you go farther in each race than the current record holder.
+
+The organizer brings you over to the area where the boat races are held. The boats are much smaller than you expected - they're actually toy boats, each with a big button on top. Holding down the button charges the boat, and releasing the button allows the boat to move. Boats move faster if their button was held longer, but time spent holding the button counts against the total race time. You can only hold the button at the start of the race, and boats don't move until the button is released.
+
+For example:
+
+Time:      7  15   30
+Distance:  9  40  200
+This document describes three races:
+
+The first race lasts 7 milliseconds. The record distance in this race is 9 millimeters.
+The second race lasts 15 milliseconds. The record distance in this race is 40 millimeters.
+The third race lasts 30 milliseconds. The record distance in this race is 200 millimeters.
+Your toy boat has a starting speed of zero millimeters per millisecond. For each whole millisecond you spend at the beginning of the race holding down the button, the boat's speed increases by one millimeter per millisecond.
+
+So, because the first race lasts 7 milliseconds, you only have a few options:
+
+Don't hold the button at all (that is, hold it for 0 milliseconds) at the start of the race. The boat won't move; it will have traveled 0 millimeters by the end of the race.
+Hold the button for 1 millisecond at the start of the race. Then, the boat will travel at a speed of 1 millimeter per millisecond for 6 milliseconds, reaching a total distance traveled of 6 millimeters.
+Hold the button for 2 milliseconds, giving the boat a speed of 2 millimeters per millisecond. It will then get 5 milliseconds to move, reaching a total distance of 10 millimeters.
+Hold the button for 3 milliseconds. After its remaining 4 milliseconds of travel time, the boat will have gone 12 millimeters.
+Hold the button for 4 milliseconds. After its remaining 3 milliseconds of travel time, the boat will have gone 12 millimeters.
+Hold the button for 5 milliseconds, causing the boat to travel a total of 10 millimeters.
+Hold the button for 6 milliseconds, causing the boat to travel a total of 6 millimeters.
+Hold the button for 7 milliseconds. That's the entire duration of the race. You never let go of the button. The boat can't move until you let go of the button. Please make sure you let go of the button so the boat gets to move. 0 millimeters.
+Since the current record for this race is 9 millimeters, there are actually 4 different ways you could win: you could hold the button for 2, 3, 4, or 5 milliseconds at the start of the race.
+
+In the second race, you could hold the button for at least 4 milliseconds and at most 11 milliseconds and beat the record, a total of 8 different ways to win.
+
+In the third race, you could hold the button for at least 11 milliseconds and no more than 19 milliseconds and still beat the record, a total of 9 ways you could win.
+
+To see how much margin of error you have, determine the number of ways you can beat the record in each race; in this example, if you multiply these values together, you get 288 (4 * 8 * 9).
+
+Determine the number of ways you could beat the record in each race. What do you get if you multiply these numbers together?
+
+
+--- Part Two ---
+As the race is about to start, you realize the piece of paper with race times and record distances you got earlier actually just has very bad kerning. There's really only one race - ignore the spaces between the numbers on each line.
+
+So, the example from before:
+
+Time:      7  15   30
+Distance:  9  40  200
+...now instead means this:
+
+Time:      71530
+Distance:  940200
+Now, you have to figure out how many ways there are to win this single race. In this example, the race lasts for 71530 milliseconds and the record distance you need to beat is 940200 millimeters. You could hold the button anywhere from 14 to 71516 milliseconds and beat the record, a total of 71503 ways!
+
+How many ways can you beat the record in this one much longer race?""")
+
+
+
+class testCase(unittest.TestCase):
+    global DEBUG
+    DEBUG = True
+
+    test_set = (
+        """Time:      7  15   30
+Distance:  9  40  200"""
+    )
+
+    def test_one_star(self):
+        self.assertEqual(
+            one_star(
+                self.__class__.test_set
+            ),
+            288
+        )
+
+    def test_two_star(self):
+        self.assertEqual(
+            two_star(
+                self.__class__.test_set
+            ),
+            71503
+        )
+
+
+
+if __name__ == '__main__':
+    try:
+        sys.argv[1]
+        puzzle_text()
+
+    except:
+        DEBUG = False
+
+        username = 'crushing'
+        m = hashlib.sha256()
+        hostname = socket.gethostname()
+        m.update(hostname.encode('utf8'))
+        if m.hexdigest() == 'ec7c98e2b47378ec88e1f9cce8d6ed91b9d616787c8a37023fd5c67cef1ff71f':
+            username = 'conrad.rushing'
+        print ('hostname str :',hostname)
+        print ('hostname hash:', m.hexdigest())
+
+        filename_script = os.path.basename(__file__)
+        print("---------------%s--------------------"%filename_script)
+        filename = filename_script.split('.')[0]
+        input_set = ()
+        
+        with open("/Users/%s/Development/crushallhumans/adventofcode2023/inputs/2023/%s.txt" % (username,filename)) as input_file:
+            input_set = [input_line.strip() for input_line in input_file]
+        ret = one_star(input_set)
+        print (ret)
+
+        ret = two_star(input_set)
+        print (ret)
